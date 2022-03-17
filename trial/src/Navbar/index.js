536d06fb9f94226cbb55f7cc66b871689d7d4413
kiritocode1 <kathawalearyan9@gmail.com>
2022-03-17 18:52:11 +0530

    gg

--- trial/src/Navbar/index.js
+++ trial/src/Navbar/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div >
-      <h1>chu chu is dum dum bum bum chum chum</h1>
+      <h1 className="font-8 text-2xl py-2">The Quick Brown Fox Jumped over the Lazy dog</h1>
     </div>
   );
   };
-
 export default Navbar;
